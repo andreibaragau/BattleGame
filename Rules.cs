ba5da33c72c0998b48ffc47309966660ff6e45ae
Andrei Baragau <andreibaragau@yahoo.com>
2022-10-26 18:24:16 +0300

    Add wizard class for point C

--- Rules.cs
+++ Rules.cs
@@ -51,6 +51,41 @@ namespace BattleGameApp
             }
 
         }
+
+        public void switchWizzard(Warrior warrior)
+        {
+            Wizard wizard = new Wizard();
+            int opt = rnd.Next(8);
+            switch (opt)
+            {
+                case 1:
+                    {
+                        wizard.RestoreHealth(warrior);
+                        Console.WriteLine("wizard restore health");
+                        break;
+                    }
+                case 2:
+                    {
+                        wizard.HealthIcrease(warrior);
+                        Console.WriteLine("wizard increase health");
+                        break;
+                    }
+                case 3:
+                    {
+                        wizard.MaxAttackIncrease(warrior);
+                        Console.WriteLine("wizard increase max attack");
+                        break;
+                    }
+                case 4:
+                    {
+                        wizard.MaxBlockIncrease(warrior);
+                        Console.WriteLine("wizard increase max block");
+                        break;
+                    }
+                default:break;
+                    
+            }
+        }
         public void Play()
         {
             warrior1.Health = 100;
@@ -65,7 +100,9 @@ namespace BattleGameApp
             while (true)
             {
                 if (round == 0)
-                {                    
+                {
+                    switchWizzard(warrior1);
+                    switchWizzard(warrior2);
                     int warrior1Attack = switchWeaponsAttack(warrior1);
                     int warrior2Block = switchWeaponsDefend(warrior2);
 
@@ -73,20 +110,22 @@ namespace BattleGameApp
                     {
                         int damage = warrior1Attack - warrior2Block;
                         warrior2.Health -= damage;
-                        Console.WriteLine("Attacker:{0}, attack value {1}, Defender: {2}, block value {3}","W1", warrior1Attack, "w2", warrior2Block);
-                        Console.WriteLine("Winner of this round {0}, damage: {1}, remaining health of the loser {2}", "w1", damage, warrior2.Health);
+                        Console.WriteLine("Attacker:{0}, attack value {1}, Defender: {2}, block value {3}","Warrior1", warrior1Attack, "Warrior2", warrior2Block);
+                        Console.WriteLine("Winner of this round {0}, damage: {1}, remaining health of the loser {2}", "Warrior1", damage, warrior2.Health);
                         Console.WriteLine("health w1->{0}, w2->{1}", warrior1.Health, warrior2.Health);
                         Console.WriteLine("***********");
                     } else if (warrior1Attack <= warrior2Block)
                     {
-                        Console.WriteLine("{0} block the attack","W2");
+                        Console.WriteLine("{0} block the attack", "Warrior2");
                         Console.WriteLine("***********");
                     }
                 
                     round = 1;                 
                 }
                 else
-                {                 
+                {
+                    switchWizzard(warrior1);
+                    switchWizzard(warrior2);
                     int warrior2Attack = switchWeaponsAttack(warrior2);
                     int warrior1Block = switchWeaponsDefend(warrior1);
 
@@ -94,14 +133,14 @@ namespace BattleGameApp
                     {
                         int damage = warrior2Attack - warrior1Block;
                         warrior1.Health -= damage;
-                        Console.WriteLine("Attacker:{0}, attack value {1}, Defender: {2}, block value {3}", "W2", warrior2Attack, "w1", warrior1Block);
-                        Console.WriteLine("Winner of this round {0}, damage: {1}, remaining health of the loser {2}", "w2", damage, warrior1.Health);
-                        Console.WriteLine("health w1->{0}, w2->{1}",warrior1.Health, warrior2.Health);
+                        Console.WriteLine("Attacker:{0}, attack value {1}, Defender: {2}, block value {3}", "Warrior2", warrior2Attack, "Warrior1", warrior1Block);
+                        Console.WriteLine("Winner of this round {0}, damage: {1}, remaining health of the loser {2}", "Warrior2", damage, warrior1.Health);
+                        Console.WriteLine("health Warrior1->{0}, Warrior2->{1}", warrior1.Health, warrior2.Health);
                         Console.WriteLine("***********");
                     }
                     else if (warrior2Attack <= warrior1Block)
                     {
-                        Console.WriteLine("{0} block the attack", "W1");
+                        Console.WriteLine("{0} block the attack", "Warrior1");
                         Console.WriteLine("***********");
                     }
                     round = 0;
@@ -110,12 +149,12 @@ namespace BattleGameApp
 
                 if (warrior1.Health > 0 && warrior2.Health <= 0) 
                 {
-                    Console.WriteLine("{0} win", "W1");
+                    Console.WriteLine("{0} win", "Warrior1");
                     return;
                 }
                 if (warrior2.Health > 0 && warrior1.Health <= 0)
                 {
-                    Console.WriteLine("{0} win", "W2");
+                    Console.WriteLine("{0} win", "Warrior2");
                     return;
                 }
                 if (warrior1.Health <= 0 && warrior2.Health <= 0)
---
+++ Wizard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleGameApp
+{
+    class Wizard:Warrior
+    {
+        
+        public void RestoreHealth(Warrior warrior)
+        {
+            warrior.Health = 100;
+        }
+        public void HealthIcrease(Warrior warrior)
+        {
+            warrior.Health += 20;
+            if(warrior.Health>100)
+            {
+                warrior.Health = 100;
+            }
+        }
+
+        public void MaxAttackIncrease(Warrior warrior)
+        {
+            warrior.MaxAttack += 10;
+        }
+        public void MaxBlockIncrease(Warrior warrior)
+        {
+            warrior.MaxBlock += 10;
+        }
+
+    }
+}
